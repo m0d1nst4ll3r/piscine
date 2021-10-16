074ee6219eae7f5281c032bcc05b0e9c6f79fc7f
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2021-10-17 00:54:12 +0200

    c03ex01 done

---
+++ days/c03/ex01/ft_strncmp.c
@@ -0,0 +1,15 @@
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
