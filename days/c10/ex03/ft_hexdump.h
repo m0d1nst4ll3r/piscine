92d9ab62d960bbe334199355cf514a88df28d142
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2021-11-01 15:37:01 +0100

    c10 ex03 started

---
+++ days/c10/ex03/ft_hexdump.h
@@ -0,0 +1,12 @@
+#ifndef FT_HEXDUMP_H
+# define FT_HEXDUMP_H
+
+# include <fnctl.h>
+# include <unist.h>
+# include <errno.h>
+# include <string.h>
+
+//	ft_hexdump_util.c
+char	*hd_realloc(char *old, char byte);
+
+#endif
